00e111ad5dc29cc8050390b3cfc3a2f610c980d8
Casaper <nikisshlife@gmail.com>
2020-07-26 19:33:20 +0300

    fef

--- wp-content/themes/vgw/footer.php
+++ wp-content/themes/vgw/footer.php
@@ -1,23 +1,51 @@
 <footer>
     <div class="container">
-        <div class="rule"></div>
-        <div class="footer-logo"><img src="/wp-content/themes/vgw/img/logo-footer.png" alt=""></div>
-        <div class="footer-text">
-            <div>© 2020 Virtuozila. Все права защищены. Все торговые марки являются собственностью
-                соответствующих владельцев в США и других странах.</div>
-            <div class="navbar-nav text-menu footer-menu-v">
-                <?php wp_nav_menu( [ 'menu' => 'footer' ] ); ?>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="footer-text">
+                    <div class="navbar-nav text-menu footer-menu-v">
+                        <?php wp_nav_menu( [ 'menu' => 'footer' ] ); ?>
+                    </div>
+                </div>
             </div>
-        <div class="social">
-        <a href="https://www.youtube.com/channel/UCbQM_PPbVELNLt-5ZAnS-Vw"><img src="/wp-content/themes/vgw/img/yt.png" alt=""></a>
-        <a href="#"><img src="/wp-content/themes/vgw/img/vk.png" alt=""></a>
-        <a href="#"><img src="/wp-content/themes/vgw/img/fb.png" alt=""></a>
-        <a href="#"><img src="/wp-content/themes/vgw/img/insta.png" alt=""></a>
-        </div>
-        </div>
 
-        <div class="rule"></div>
-        
+            <div class="col-md-4">
+                <div class="adres">
+                    <p>04655, г.Киев, пр-т Степана Бендеры, 21</p>
+                </div>
+                <div class="contacts-footer">
+                    <p>Отдел реализации
+                        +38 (044) 467-76-21; 425-31-81;
+                        marketing@medaparatura.kiev.ua</p>
+
+                    <p>Отдел маркетинга
+                        +38 (044) 467-76-21
+                        marketing@medaparatura.kiev.ua
+                    </p>
+                    <p>Сервисная служба
+                        +38 (044) 467-76-26
+                        service@medaparatura.kiev.ua</p>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="tel-footer">
+                    <p><i></i>(044) 467 76 21</p>
+                    <p><i></i>(044) 467 76 21</p>
+                </div>
+                <div>
+                    <p>Торговый дом “Медаппаратура”
+                        +38 (044) 490-35-41
+                        trade@medaparatura.kiev.ua</p>
+
+                    <p>Торговый дом “Медаппаратура”
+                        +38 (044) 490-35-41
+                        trade@medaparatura.kiev.ua</p>
+                </div>
+            </div>
+        </div>
+        <div>2020 © Киевское производственное объединение “Медаппаратура”</div>
+        <p>Сайт разработан Casper WDC</p>
     </div>
 </footer>
 
--- wp-content/themes/vgw/page-home-page.php
+++ wp-content/themes/vgw/page-home-page.php
@@ -120,60 +120,92 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col-12 col-sm-12 col-md-6">
-                <div class="tomos">    
-                <div class="tomosintez">
-                        <h2>томосинтез</h2>
-                    </div>
-                    <div class="tomo-spisok">
-                        <ul>
-                            <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="">Почему датчик это важно?</a></li>
-                            <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="">Рекомендации по очистке и дезинфекции ультразвуковых датчиков</a></li>
-                            <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="">Почему датчик это важно?</a></li>
-                            <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="">Рекомендации по очистке и дезинфекции ультразвуковых датчиков</a></li>
-                            <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="">Почему датчик это важно?</a></li>
-                            <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="">Рекомендации по очистке и дезинфекции ультразвуковых датчиков</a></li>
-                            <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="">Почему датчик это важно?</a></li>
-                        </ul>
-                    </div>
+                    <div class="tomos">
+                        <div class="tomosintez">
+                            <h2>томосинтез</h2>
+                        </div>
+                        <div class="tomo-spisok">
+                            <ul>
+                                <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="#">Почему датчик
+                                        это важно?</a></li>
+                                <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="#">Рекомендации
+                                        по
+                                        очистке и дезинфекции ультразвуковых датчиков</a></li>
+                                <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="#">Почему датчик
+                                        это важно?</a></li>
+                                <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="#">Рекомендации
+                                        по
+                                        очистке и дезинфекции ультразвуковых датчиков</a></li>
+                                <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="#">Почему датчик
+                                        это важно?</a></li>
+                                <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="#">Рекомендации
+                                        по
+                                        очистке и дезинфекции ультразвуковых датчиков</a></li>
+                                <li><i class="fa fa-question-circle-o" aria-hidden="true"></i><a href="#">Почему датчик
+                                        это важно?</a></li>
+                            </ul>
+                        </div>
+                        <div class="btn-blog">
+                            <a href="#" class="all-news-btn"> Смотреть все статьи <i class="fa fa-long-arrow-right"
+                                    aria-hidden="true"></i></a>
+                        </div>
                     </div>
                 </div>
                 <div class="col-12 col-sm-12 col-md-6">
                     <div class="news-block">
-                    <div class="news-tetle">
-                        <h2>Новости</h2>
-                    </div>
-                    <div>
-                        <?php $catquery = new WP_Query( 'posts_per_page=3' ); ?>
-                        <ul class="post-block">
-                            <?php while($catquery->have_posts()) : $catquery->the_post(); ?>
-                            <li class="post-item">
-                                <div class="post-content">
-                                    <div class="post-image"><a
-                                            href="<?php the_permalink() ?>"><?php the_post_thumbnail(array(200, 115)); ?></a>
-                                    </div>
-                                    <div>
-                                    <h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-                                    <p class="data"><?php the_date(); ?></p>
-                                    </div>
-                                </div>
-
-                            </li>
-                            <?php endwhile; ?>
-                        </ul>
-                        <?php wp_reset_postdata(); ?>
-                        <div class="btn-blog">
-                            <a href="/blog/" class="all-news-btn"> Все новости <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                        <div class="news-tetle">
+                            <h2>Новости</h2>
                         </div>
-                    </div>
+                        <div>
+                            <?php $catquery = new WP_Query( 'posts_per_page=3' ); ?>
+                            <ul class="post-block">
+                                <?php while($catquery->have_posts()) : $catquery->the_post(); ?>
+                                <li class="post-item">
+                                    <div class="post-content">
+                                        <div class="post-image"><a
+                                                href="<?php the_permalink() ?>"><?php the_post_thumbnail(array(200, 115)); ?></a>
+                                        </div>
+                                        <div>
+                                            <h3><a href="<?php the_permalink() ?>"
+                                                    rel="bookmark"><?php the_title(); ?></a></h3>
+                                            <p class="data"><?php the_date(); ?></p>
+                                        </div>
+                                    </div>
+
+                                </li>
+                                <?php endwhile; ?>
+                            </ul>
+                            <?php wp_reset_postdata(); ?>
+                            <div class="btn-blog">
+                                <a href="/blog/" class="all-news-btn"> Все новости <i class="fa fa-long-arrow-right"
+                                        aria-hidden="true"></i></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
-
         </div>
     </section>
     <!--Blog  END-->
 
+    <!--Form-->
+    <section class="block-6">
+        <div class="container">
+            <div class="row help">
+                <p>Нужна помощь в подборе оборудования?</p>
+                <div class="form-help">
+                    <?php echo do_shortcode( '[fluentform id="2"]' ); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Form END-->
+
+     <!--map-->
+    <section class="block-7">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1036.8859788244258!2d30.49283137759125!3d50.48936577993065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cdfbc6f2f15b%3A0xd6f3d4d1c9d19e9c!2z0L_RgNC-0YHQv9C10LrRgiDQodGC0LXQv9Cw0L3QsCDQkdCw0L3QtNC10YDRiywgMjEsINCa0LjQtdCyLCAwMjAwMA!5e0!3m2!1sru!2sua!4v1595777716251!5m2!1sru!2sua" width="100%" height="320" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </section>
+    <!--map END-->
     <?php
 get_sidebar();
 get_footer();
